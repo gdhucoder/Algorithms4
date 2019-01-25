9a0679fa46c8ad288dc5dbdec55d10b383c305cd
guodonghu <guodong_hu@126.com>
2019-01-26 07:07:39 +0800

    5.1 string sort

--- Ch_5_1/Ex_5_1_02.java
+++ Ch_5_1/Ex_5_1_02.java
@@ -8,36 +8,33 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Ex_5_1_02 {
 
-  public static class _LSD{
-    public static void sort(String a[], int W) {
+  public static class _LSD {
+
+    public static void sort(String[] a, int w) {
       int N = a.length;
       int R = 256;
       String[] aux = new String[N];
-
-      for (int d = W - 1; d >= 0; d--) {
+      for (int d = w - 1; d >= 0; d--) { // sort by key-indexed counting on dth char
         show(a);
         int[] count = new int[R + 1];
         for (int i = 0; i < N; i++) {
-          count[a[i].charAt(d) + 1]++;
+          count[a[i].charAt(d) + 1]++; // compute frequency counts
         }
-
         for (int r = 0; r < R; r++) {
-          count[r + 1] += count[r];
+          count[r + 1] += count[r]; // transforms counts to indices
         }
-
         for (int i = 0; i < N; i++) {
-          aux[count[a[i].charAt(d)]++] = a[i];
+          aux[count[a[i].charAt(d)]++] = a[i]; // distribute
         }
-
         for (int i = 0; i < N; i++) {
-          a[i] = aux[i];
+          a[i] = aux[i]; // copy back
         }
       }
     }
 
-    public static void show(Object[] a){
-      for(Object o : a){
-        StdOut.print(o+"\t");
+    public static void show(Object[] a) {
+      for (Object o : a) {
+        StdOut.print(o + "\t");
       }
       StdOut.println();
     }
