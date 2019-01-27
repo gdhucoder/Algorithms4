8c4b966b020e1895bc637b72b49e6c64aa8c73be
guodonghu <guodong_hu@126.com>
2019-01-28 06:27:04 +0800

    1.1

---
+++ Ch_1_1/Ex_1_01.java
@@ -0,0 +1,26 @@
+package Ch_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by HuGuodong on 2019/1/28.
+ */
+
+public class Ex_1_01 {
+
+  public static void main(String[] args) {
+
+    // 转换为int
+    StdOut.println((0 + 15) / 2);
+
+    // double 类型的运算
+    StdOut.println(2.0e-6 * 100000000.1);
+
+    // && 的优先级 大于 ||
+    StdOut.println(true && false || true && true);
+
+//    7
+//    200.0000002
+//    true
+  }
+}
