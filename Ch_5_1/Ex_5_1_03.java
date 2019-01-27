7923320361c42abe91eb8a1998f845b62be483fa
guodonghu <guodong_hu@126.com>
2019-01-28 05:00:43 +0800

    5.1 string sort

--- Ch_5_1/Ex_5_1_03.java
+++ Ch_5_1/Ex_5_1_03.java
@@ -1,6 +1,5 @@
 package Ch_5_1;
 
-import edu.princeton.cs.algs4.Alphabet;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
@@ -16,7 +15,7 @@ public class Ex_5_1_03 {
 
     private static int charAt(String s, int d) {
       if (d < s.length()) {
-        return _Alphabet.LOWERCASE.toIndices(s)[d];
+        return _Alphabet.LOWERCASE.toIndex(s.charAt(d));
       } else {
         return -1;
       }
@@ -32,6 +31,8 @@ public class Ex_5_1_03 {
      * sort from a[lo] to a[hi], starting at each dth character.
      */
     private static void sort(String[] a, int lo, int hi, int d) {
+
+
       if (hi <= lo) {
         return;
       }
@@ -48,13 +49,17 @@ public class Ex_5_1_03 {
       for (int i = lo; i <= hi; i++) {
         int idx = count[charAt(a[i], d) + 1]++;
         aux[idx] = a[i]; // idx 从0开始
-        System.out.println("idx: " + idx + "," + aux[idx]);
+//        System.out.println("idx: " + idx + "," + aux[idx]);
       }
 
       for (int i = lo; i <= hi; i++) {
-        System.out.println("i: " + i + ", :" + (i - lo));
+//        System.out.println("i: " + i + ", :" + (i - lo));
         a[i] = aux[i - lo]; // i-lo 对应从0开始
       }
+
+      // show trace 显示轨迹
+      StdOut.printf("sort(a, %2d, %2d, %2d): ", lo, hi, d);
+      show(a);
 
       for (int r = 0; r < R; r++) {
         sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
@@ -64,21 +69,35 @@ public class Ex_5_1_03 {
 
   }
 
+
+  /**
+   * 5_1_03
+   */
+  public static void testSort(){
+    String str = "no is th ti fo al go pe to co to th ai of th pa";
+    String[] a = str.split(" ");
+    show(a);
+    _MSD.sort(a);
+    show(a);
+  }
+
+  public static void show(String[] a){
+    for (String s : a) {
+      StdOut.print(s + "\t");
+    }
+    StdOut.println();
+  }
+
   public static void main(String[] args) {
-    String str = "she, shells, seashells, by, the, sea, shore, the, shells, she, shells, are, surely, seashells";
-    String[] strings = str.replaceAll(" ", "").split(",");
+    testSort();
 
-    for (String s : strings) {
-      StdOut.print(s + "\t");
-    }
-    StdOut.println();
-
-    _MSD.sort(strings);
-
-    for (String s : strings) {
-      StdOut.print(s + "\t");
-    }
-    StdOut.println();
-
+//    no	is	th	ti	fo	al	go	pe	to	co	to	th	ai	of	th	pa
+//    sort(a,  0, 15,  0): al	ai	co	fo	go	is	no	of	pe	pa	th	ti	to	to	th	th
+//    sort(a,  0,  1,  1): ai	al	co	fo	go	is	no	of	pe	pa	th	ti	to	to	th	th
+//    sort(a,  8,  9,  1): ai	al	co	fo	go	is	no	of	pa	pe	th	ti	to	to	th	th
+//    sort(a, 10, 15,  1): ai	al	co	fo	go	is	no	of	pa	pe	th	th	th	ti	to	to
+//    sort(a, 10, 12,  2): ai	al	co	fo	go	is	no	of	pa	pe	th	th	th	ti	to	to
+//    sort(a, 14, 15,  2): ai	al	co	fo	go	is	no	of	pa	pe	th	th	th	ti	to	to
+//    ai	al	co	fo	go	is	no	of	pa	pe	th	th	th	ti	to	to
   }
 }
