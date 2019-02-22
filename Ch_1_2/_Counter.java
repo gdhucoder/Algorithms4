70c7d8a468d3d330c4b15cfbe61149e99e67dd9b
guodonghu <guodong_hu@126.com>
2019-02-22 08:13:01 +0800

    1.2

--- Ch_1_2/_Counter.java
+++ Ch_1_2/_Counter.java
@@ -1,8 +1,70 @@
 package Ch_1_2;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * Created by HuGuodong on 2019/2/22.
  */
 
 public class _Counter {
+
+  private final String name; // counter name
+  private int count = 0; // current value
+
+  /**
+   * initializes a new counter starting at 0, with the given id.
+   * @param id the name of the counter
+   */
+  public _Counter(String id) {
+    this.name = id;
+  }
+
+  /**
+   * increment the counter by one
+   */
+  public void increment() {
+    count++;
+  }
+
+  /**
+   * number of increments since creation
+   */
+  public int tally() {
+    return count;
+  }
+
+  public String toString() {
+    return count + " " + name;
+  }
+
+
+  /**
+   * create n counters
+   * increment trials counters at random and print results
+   * @param args
+   */
+  public static void main(String[] args) {
+    int numCounters = 2;
+    int trials = 10000;
+
+    // create n counters
+    _Counter[] hits = new _Counter[numCounters];
+    for (int i = 0; i < numCounters; i++) {
+      hits[i] = new _Counter("counter " + i);
+    }
+
+    // increment trials counters at random
+    for (int i = 0; i < trials; i++) {
+      hits[StdRandom.uniform(numCounters)].increment();
+    }
+
+    // print results
+    for (int i = 0; i < numCounters; i++) {
+      StdOut.println(hits[i]);
+    }
+
+  }
+
+
 }
--- Ch_1_2/_Flips.java
+++ Ch_1_2/_Flips.java
@@ -1,8 +1,28 @@
 package Ch_1_2;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * Created by HuGuodong on 2019/2/22.
  */
 
 public class _Flips {
+
+  public static void main(String[] args) {
+    int trials = 100;
+    _Counter heads = new _Counter("heads");
+    _Counter tails = new _Counter("tails");
+    for (int i = 0; i < trials; i++) {
+      if (StdRandom.bernoulli(0.5)) {
+        heads.increment();
+      } else {
+        tails.increment();
+      }
+    }
+    StdOut.println(heads);
+    StdOut.println(tails);
+    int delta = heads.tally() - tails.tally();
+    StdOut.println("delta: " + Math.abs(delta));
+  }
 }
