719ea52ce0e666440473ee9b837c177ac14a9c0a
guodonghu <guodong_hu@126.com>
2019-04-20 18:15:56 +0800

    counting sort

--- geekbang/u13/CountingSort.java
+++ geekbang/u13/CountingSort.java
@@ -67,6 +67,10 @@ public class CountingSort {
     sort(a);
     StdOut.println("after sort");
     StdOut.println(Arrays.toString(a));
+//    count: [2, 2, 4, 7, 7, 8]
+//    sort: [0, 0, 2, 2, 3, 3, 3, 5]
+//    after sort
+//    [0, 0, 2, 2, 3, 3, 3, 5]
   }
 }
  
