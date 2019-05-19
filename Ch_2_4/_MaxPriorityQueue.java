3b3f3a8d965e30191d0e2dc6665b22ace56dc8fd
guodonghu <guodong_hu@126.com>
2019-05-19 23:30:49 +0800

    binary heap

---
+++ Ch_2_4/_MaxPriorityQueue.java
@@ -0,0 +1,110 @@
+package Ch_2_4;
+
+import edu.princeton.cs.algs4.StdRandom;
+import java.util.Arrays;
+import tools.ArrayGenerator;
+
+/**
+ * Created by HuGuodong on 2019/5/19.
+ */
+
+public class _MaxPriorityQueue<Key extends Comparable<Key>> {
+
+  private Key[] pq;
+  private int N;
+
+  /**
+   * create a priority queue
+   */
+  public _MaxPriorityQueue() {
+
+  }
+
+  /**
+   * create a priority queue of initial capacity max
+   */
+  public _MaxPriorityQueue(int max) {
+    pq = (Key[]) new Comparable[max + 1];
+    N = 0;
+  }
+
+  public void insert(Key v) {
+    pq[++N] = v;
+    swim(N);
+  }
+
+
+  public Key max() {
+    if (isEmpty()) {
+      throw new IllegalStateException("qp is empty!");
+    }
+    return pq[1];
+  }
+
+  public Key delMax() {
+    if (isEmpty()) {
+      throw new IllegalStateException("qp is empty!");
+    }
+    Key max = pq[1];
+    swap(1, N);
+    pq[N] = null;
+    N--;
+    sink(1);
+    return max;
+  }
+
+  public boolean isEmpty() {
+    return N == 0;
+  }
+
+  public int size() {
+    return N;
+  }
+
+  private void swim(int k) {
+    while (k > 1 && less(k / 2, k)) {
+      swap(k / 2, k);
+      k /= 2;
+    }
+  }
+
+  private void sink(int k) {
+    while (k * 2 < N) {
+      int j = 2 * k;
+      if (j < N && less(j, j + 1)) {
+        j++;
+      }
+      if (!less(k, j)) {
+        break;
+      }
+      swap(k, j);
+      k = j;
+    }
+  }
+
+  private boolean less(int i, int j) {
+    return pq[i].compareTo(pq[j]) < 0;
+  }
+
+  private void swap(int i, int j) {
+    Key temp = pq[i];
+    pq[i] = pq[j];
+    pq[j] = temp;
+  }
+
+  public static void main(String[] args) {
+    String[] s = "T S R P N O A E I H G".split(" ");
+//    StdRandom.shuffle(s);
+    System.out.println(Arrays.toString(s));
+    _MaxPriorityQueue<String> pq = new _MaxPriorityQueue<>(s.length);
+    for (int i = 0; i < s.length; i++) {
+      pq.insert(s[i]);
+    }
+    System.out.println(Arrays.toString(pq.pq));
+    while (!pq.isEmpty()) {
+      System.out.println(pq.delMax());
+    }
+
+
+  }
+}
--- Ch_2_4/note.md
+++ Ch_2_4/note.md
@@ -1,13 +1,18 @@
 PRIORITY QUEUES 优先队列
 
-Binary Heap: key in each node is larger than or equal to the keys in that's node's two children (if any)
+Heap: key in each node is larger than or equal to the keys in that's node's two children (if any)
 
-Representation:
+Representation: linked list, array
 
-linked list
-
-array
+![2019-05-19-002](https://gitee.com/gdhu/prvpic/raw/master/2019-05-19-002.jpg)
 
 
+![2019-05-19-003](https://gitee.com/gdhu/prvpic/raw/master/2019-05-19-003.jpg)
+
+Binary Heap: a binary heap is a collection of keys arranged in a complete heap-ordered binary
+tree, represented in level order in an array (not using the first entry)
 
 
+Operation: insert and remove the maximum
+
+![operation](https://gitee.com/gdhu/prvpic/raw/master/2019-05-19-004.jpg)
