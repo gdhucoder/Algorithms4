7399d2d8d0179658af93f3f1c1f88df974b9936a
guodonghu <guodong_hu@126.com>
2019-06-11 15:59:07 +0800

    关于基本类型的转型计算

---
+++ Ch_1_1/Ex_1_1_34_3.java
@@ -0,0 +1,20 @@
+package Ch_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by HuGuodong on 2019/6/11.
+ */
+
+public class Ex_1_1_34_3 {
+
+  /**
+   * 关于基本类型的转型计算
+   * @param args
+   */
+  public static void main(String[] args) {
+//    double avg = (double) (2 / 3); // 整型2/3 结果是0，所以会输出0.0
+    double avg = (double) 2 / 3;// 将2转型是double，会输出0.666666
+    StdOut.println(avg);
+  }
+}
