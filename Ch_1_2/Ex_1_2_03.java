ef0437788f607681afdb3610e0d57827903e64d0
guodonghu <guodong_hu@126.com>
2019-06-17 21:57:16 +0800

    1.2.03 题目说明

---
+++ Ch_1_2/Ex_1_2_03.java
@@ -0,0 +1,15 @@
+package Ch_1_2;
+
+/**
+ * Created by HuGuodong on 2019/6/17.
+ */
+
+public class Ex_1_2_03 {
+
+
+
+//  Write an Interval2D client that takes command-line arguments N, min, and max and generates N random 2D intervals whose width and height are uniformly distributed between min and max in the unit square. Draw them on StdDraw and print the number of pairs of intervals that intersect and the number of intervals that are contained in one another.
+  public static void main(String[] args) {
+
+  }
+}
