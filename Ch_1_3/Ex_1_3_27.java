a6374ebd3435dc458c65f2f1a1977e11eb9846ba
guodonghu <guodong_hu@126.com>
2019-07-20 08:07:17 +0800

    1.3.27

---
+++ Ch_1_3/Ex_1_3_27.java
@@ -0,0 +1,62 @@
+package Ch_1_3;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by HuGuodong on 2019/7/20.
+ */
+
+public class Ex_1_3_27 {
+
+  public static class Node<T> {
+
+    T item;
+    Node next;
+
+    public Node(T item) {
+      this.item = item;
+    }
+
+    public Node append(T item) {
+      Node x = new Node(item);
+      this.next = x;
+      return x;
+    }
+  }
+
+  public static void print(Node cur) {
+    while (cur != null) {
+      StdOut.printf("%s ", cur.item);
+      cur = cur.next;
+    }
+    StdOut.println();
+  }
+
+  public static int max(Node<Integer> n) {
+    Integer max = 0;
+    Node<Integer> cur = n;
+    while (cur != null) {
+      if (cur.item.compareTo(max) > 0) {
+        max = cur.item;
+      }
+      cur = cur.next;
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    Node<Integer> first = new Node<>(1);
+    first.append(2)
+        .append(3)
+        .append(3)
+        .append(3)
+        .append(3)
+        .append(4)
+        .append(10)
+        .append(12)
+        .append(30)
+        .append(1);
+    print(first);
+    StdOut.println(max(first));
+  }
+}
