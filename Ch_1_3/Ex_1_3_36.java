d44d7ef2bc2eb522567a0dc2e24d7ccf85d58a58
guodonghu <guodong_hu@126.com>
2019-07-27 12:00:51 +0800

    1.3.36

--- Ch_1_3/Ex_1_3_36.java
+++ Ch_1_3/Ex_1_3_36.java
@@ -2,14 +2,15 @@ package Ch_1_3;
 
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import java.util.Iterator;
 
 /**
- * Created by HuGuodong on 2019/7/24.
+ * Created by HuGuodong on 2019/7/27.
  */
 
-public class Ex_1_3_35 {
+public class Ex_1_3_36 {
 
-  public static class _RandomQueue<Item> {
+  public static class _RandomQueue<Item> implements Iterable<Item> {
 
     int N;
     Item[] a;
@@ -60,6 +61,42 @@ public class Ex_1_3_35 {
     public int size() {
       return N;
     }
+
+    public class RandomIterator implements Iterator<Item> {
+
+      Item[] copy;
+      int num = N;
+      int idx;
+
+      public RandomIterator() {
+        copy = (Item[]) new Object[num];
+        for (int i = 0; i < num; i++) {
+          copy[i] = a[i];
+        }
+        for (int i = 0; i < num; i++) {
+          int r = i + StdRandom.uniform(num - i); // between i~n-1
+          Item temp = copy[i];
+          copy[i] = copy[r];
+          copy[r] = temp;
+        }
+      }
+
+      @Override
+      public boolean hasNext() {
+        return idx < num;
+      }
+
+      @Override
+      public Item next() {
+        return copy[idx++];
+      }
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+      return new RandomIterator();
+    }
+
   }
 
   public static void main(String[] args) {
@@ -68,15 +105,10 @@ public class Ex_1_3_35 {
     for (int i = 0; i < N; i++) {
       q.enqueue(i);
     }
-    // sample
-    for (int i = 0; i < N; i++) {
-      StdOut.printf("%d ", q.sample());
+    for (Integer i : q) {
+      StdOut.printf("%d ", i);
     }
     StdOut.println();
-    // dequeue
-    while (!q.isEmpty()) {
-      StdOut.printf("%d ", q.dequeue());
-    }
-    StdOut.println();
+//    7 10 0 5 12 4 15 6 18 3 19 17 11 14 16 1 13 2 9 8
   }
 }
