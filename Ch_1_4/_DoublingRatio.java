482f2e598f6ea0ec6e737e0087fbce4393466bd9
gdhu <guodong_hu@126.com>
2019-08-11 23:21:42 +0800

    double ratio

---
+++ Ch_1_4/_DoublingRatio.java
@@ -0,0 +1,27 @@
+package Ch_1_4;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class _DoublingRatio {
+    public static double timeTrial(int n){
+        return _ThreeSumDoublingTest.timeTrial(n);
+    }
+
+    public static void main(String[] args) {
+        double prev = timeTrial(125);
+        for (int n = 250; true; n+=n) {
+            double time = timeTrial(n);
+            StdOut.printf("%6d, %7.1f", n, time);
+            StdOut.printf("%5.1f\n", time/prev);
+            prev = time;
+        }
+//        250,     0.0  1.0
+//        500,     0.0  3.0
+//        1000,     0.0  2.7
+//        2000,     0.1  4.1
+//        4000,     0.3  4.1
+//        8000,     1.1  3.9
+//        16000,     4.2  4.0
+//        32000,    17.9  4.2
+    }
+}
