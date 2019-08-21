1b552bf61c5de7b3269b67b8630be24fe6de2e09
gdhu <guodong_hu@126.com>
2019-08-21 21:50:17 +0800

    format code

--- Ch_1_5/Ex_1_5_11.java
+++ Ch_1_5/Ex_1_5_11.java
@@ -10,12 +10,14 @@ import java.io.FileNotFoundException;
  * Created by HuGuodong on 2019-08-21.
  */
 public class Ex_1_5_11 {
-  public static class _WeightedQuickFindUF{
+
+  public static class _WeightedQuickFindUF {
+
     int[] id;
     int[] sz;
     int count;
 
-    public _WeightedQuickFindUF(int N){
+    public _WeightedQuickFindUF(int N) {
       id = new int[N];
       count = N;
       for (int i = 0; i < N; i++) {
@@ -27,33 +29,34 @@ public class Ex_1_5_11 {
       }
     }
 
-    public boolean connected(int p,int q){
+    public boolean connected(int p, int q) {
       return id[p] == id[q];
     }
 
-    public int find(int p){
+    public int find(int p) {
       return id[p];
     }
 
-    public void union(int p,int q){
+    public void union(int p, int q) {
       int pId = id[p];
       int qId = id[q];
-      if(sz[p]<sz[q]){
+      if (sz[p] < sz[q]) {
         for (int i = 0; i < id.length; i++) {
-          if(id[i]==pId)
+          if (id[i] == pId)
             id[i] = qId;
         }
         sz[q] += sz[p];
-      }else{
+      } else {
         for (int i = 0; i < id.length; i++) {
-          if(id[i] == qId)
+          if (id[i] == qId)
             id[i] = pId;
         }
         sz[p] += sz[q];
       }
       count--;
     }
-    public int count(){
+
+    public int count() {
       return count;
     }
   }
@@ -71,7 +74,7 @@ public class Ex_1_5_11 {
       uf.union(p, q);
       StdOut.printf("%d %d\n", p, q);
     }
-    StdOut.println(uf.count() + " components, elapsed time :" + time.elapsedTime() +"s");
+    StdOut.println(uf.count() + " components, elapsed time :" + time.elapsedTime() + "s");
 
 
   }
