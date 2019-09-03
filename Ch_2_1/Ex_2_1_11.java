867bd09b2bb1a12faa97ce51a5ba90e798c3db03
gdhu <guodong_hu@126.com>
2019-09-03 23:15:09 +0800

    2.1.11

---
+++ Ch_2_1/Ex_2_1_11.java
@@ -0,0 +1,34 @@
+package Ch_2_1;
+
+import javax.swing.JFormattedTextFieldBeanInfo;
+import tools.ArrayGenerator;
+import tools.PrintUtil;
+
+/**
+ * Created by HuGuodong on 2019-09-03.
+ */
+public class Ex_2_1_11 extends _Example {
+
+  public static void sort(Comparable[] a) {
+    int N = a.length;
+    int[] hs = new int[N / 3];
+    for (int i = 0; i < hs.length; i++) {
+      hs[i] = 3 * i + 1;
+    }
+    for (int i = hs.length - 1; i >= 0; i--) {
+      int h = hs[i]; // h sort
+      for (int j = h; j < N; j++) {
+        for (int k = j; k >= h && less(a[k], a[k - h]); k -= h) {
+          exch(a, k, k - h);
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Integer[] a = ArrayGenerator.randomIntArray(10);
+    PrintUtil.show(a);
+    sort(a);
+    PrintUtil.show(a);
+  }
+}
