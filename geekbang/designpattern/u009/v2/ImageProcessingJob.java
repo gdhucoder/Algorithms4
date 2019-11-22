d008b63fbed08e9226e241cdd4f94e0937a853ab
gdhu <guodong_hu@126.com>
2019-11-22 08:22:19 +0800

    revice

--- geekbang/designpattern/u009/v2/ImageProcessingJob.java
+++ geekbang/designpattern/u009/v2/ImageProcessingJob.java
@@ -10,11 +10,12 @@ public class ImageProcessingJob {
 
   private static final String BUCKET_NAME = "ai_images_bucket";
   private static final String STORE_TYPE = "STORE_TYPE";
+  private static final String PROP_PATH = "geekbang/designpattern/u009/v2/config.properties";
 
   public void process() {
     Image image = null;
 //    String storeType = prop.getString(STORE_TYPE);
-    String storeType = FileUtils.load("geekbang/designpattern/u009/v2/config.properties")
+    String storeType = FileUtils.load(PROP_PATH)
         .getProperty(STORE_TYPE);
     ImageStore imageStore = ImageStoreFactory.newInstance(storeType);
     imageStore.upload(image, BUCKET_NAME);
