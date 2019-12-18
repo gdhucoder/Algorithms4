0e1cb86112a3cfcabd0e618364f508c44f7d4e5c
gdhu <guodong_hu@126.com>
2019-12-18 22:11:14 +0800

    leet code 66 plus one

---
+++ leetcode/editor/cn/PlusOne.java
@@ -0,0 +1,68 @@
+//给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。 
+//
+// 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。 
+//
+// 你可以假设除了整数 0 之外，这个整数不会以零开头。 
+//
+// 示例 1: 
+//
+// 输入: [1,2,3]
+//输出: [1,2,4]
+//解释: 输入数组表示数字 123。
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,3,2,1]
+//输出: [4,3,2,2]
+//解释: 输入数组表示数字 4321。
+// 
+// Related Topics 数组
+
+package leetcode.editor.cn;
+
+import tools.PrintUtil;
+
+public class PlusOne {
+
+  public static void main(String[] args) {
+    Solution solution = new PlusOne().new Solution();
+    int[] a = {0};
+    PrintUtil.show(solution.plusOne(a));
+  }
+
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+
+    public int[] plusOne(int[] digits) {
+
+      int n = digits.length - 1;
+
+      // check the last element
+      if (digits[n] < 9) {
+        digits[n]++;
+        return digits;
+      }
+
+      // if the last element is 9, move to front
+      while (n >= 0 && digits[n] + 1 == 10) {
+        digits[n] = 0;
+        n--;
+      }
+      if (n == -1) { // already at -1, it means that 0, 0, 0, ...
+        int[] a = new int[digits.length + 1];
+        // do not need because all elements are zeros
+        // System.arraycopy(digits, 0, a, 1, digits.length);
+        a[0] = 1;
+        return a;
+      } else {
+        digits[n]++; // n is not at index 0 position
+        return digits;
+      }
+
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
