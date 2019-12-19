229c5aa2263091737bd5b9d84ffe201649f833a6
gdhu <guodong_hu@126.com>
2019-12-19 10:26:19 +0800

    leet code 66 plus one

--- leetcode/editor/cn/RotateArray.java
+++ leetcode/editor/cn/RotateArray.java
@@ -44,8 +44,7 @@ public class RotateArray {
   public class Solution {
 
     public void rotate(int[] nums, int k) {
-      if (nums.length < 2)
-        return;
+      k %= nums.length;
       reverse(nums, 0, nums.length - 1);
       reverse(nums, 0, k - 1);
       reverse(nums, k, nums.length - 1);
