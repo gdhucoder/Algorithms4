881bffd7f714c0fc71cd921a5b8f87daaa1f201f
gdhu <guodong_hu@126.com>
2019-12-19 15:23:47 +0800

    leet code 1 two sum

--- leetcode/editor/cn/TwoSum.java
+++ leetcode/editor/cn/TwoSum.java
@@ -13,6 +13,8 @@
 
 package leetcode.editor.cn;
 
+import java.util.HashMap;
+
 public class TwoSum {
 
   public static void main(String[] args) {
@@ -24,6 +26,14 @@ public class TwoSum {
   class Solution {
 
     public int[] twoSum(int[] nums, int target) {
+      HashMap<Integer, Integer> numMap = new HashMap<>();
+      for (int i = 0; i < nums.length; i++) {
+        if (numMap.containsKey(target - nums[i])) {
+          return new int[]{i, numMap.get(target - nums[i])};
+        } else {
+          numMap.put(nums[i], i);
+        }
+      }
       return null;
     }
   }
