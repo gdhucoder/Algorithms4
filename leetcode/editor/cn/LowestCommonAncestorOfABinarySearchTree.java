f059cbe6bd17089ba4e45b0b79a1b27028ed4b2a
gdhu <guodong_hu@126.com>
2019-12-26 06:18:52 +0800

    leet code 235 & 236

---
+++ leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,63 @@
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+//输出: 6 
+//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+//输出: 2
+//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉搜索树中。 
+// 
+// Related Topics 树
+
+package leetcode.editor.cn;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+
+  public static void main(String[] args) {
+    Solution solution = new LowestCommonAncestorOfABinarySearchTree().new Solution();
+  }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+  /**
+   * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+   * right; TreeNode(int x) { val = x; } }
+   */
+  class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+      if (root == null)
+        return null;
+      if (p.val > root.val && q.val > root.val)
+        return lowestCommonAncestor(root.right, p, q);
+      else if (p.val < root.val && q.val < root.val)
+        return lowestCommonAncestor(root.left, p, q);
+      else
+        return root;
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcode/editor/cn/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,64 @@
+//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉树: root = [3,5,1,6,2,0,8,null,null,7,4] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出: 3
+//解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出: 5
+//解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉树中。 
+// 
+// Related Topics 树
+
+package leetcode.editor.cn;
+
+public class LowestCommonAncestorOfABinaryTree {
+
+  public static void main(String[] args) {
+    Solution solution = new LowestCommonAncestorOfABinaryTree().new Solution();
+  }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+  /**
+   * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+   * right; TreeNode(int x) { val = x; } }
+   */
+  class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+      if (root == null)
+        return null;
+      if (root == p || root == q)
+        return root;
+      TreeNode left = lowestCommonAncestor(root.left, p, q);
+      TreeNode right = lowestCommonAncestor(root.right, p, q);
+      return left == null ? right :
+          right == null ? left : root;
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
