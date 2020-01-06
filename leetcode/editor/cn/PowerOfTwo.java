8eb5eee9b21d0e2680ddf2054a2ab04d3c180c29
gdhu <guodong_hu@126.com>
2020-01-07 07:36:48 +0800

    leet code 231

--- leetcode/editor/cn/PowerOfTwo.java
+++ leetcode/editor/cn/PowerOfTwo.java
@@ -31,7 +31,10 @@ public class PowerOfTwo {
   class Solution {
 
     public boolean isPowerOfTwo(int n) {
-      return false;
+      while (n > 0 && n % 2 == 0) {
+        n = n / 2;
+      }
+      return n == 1;
     }
   }
 //leetcode submit region end(Prohibit modification and deletion)
