e8ad2f0eb253607d85e6af7929721a7ecd86d258
gdhu <guodong_hu@126.com>
2020-01-12 23:40:49 +0800

    leet code weekly competition 171

---
+++ ALeetCode/comp/p171/Problem2.java
@@ -0,0 +1,40 @@
+package ALeetCode.comp.p171;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem2 {
+
+  static class Solution {
+
+    public int minFlips(int a, int b, int c) {
+      int res = 0;
+      for (int i = 0; i < 9; i++) {
+        int bit_c = c >> i & 1;
+        int bit_a = a >> i & 1;
+        int bit_b = b >> i & 1;
+        if (bit_c == 0) {
+          if (bit_a == 1)
+            res += 1;
+          if (bit_b == 1)
+            res += 1;
+        } else {
+          if (bit_a == 0 && bit_b == 0) {
+            res += 1;
+          }
+        }
+
+      }
+      return res;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.minFlips(1, 2, 3));
+//    int a = 4;
+//    for (int i = 0; i < 32; i++) {
+//      System.out.println(a >> i & 1);
+//    }
+  }
+}
---
+++ ALeetCode/comp/p171/Problem3.java
@@ -0,0 +1,30 @@
+package ALeetCode.comp.p171;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem3 {
+
+  static class Solution {
+
+    public int makeConnected(int n, int[][] connections) {
+      if (n - 1 > connections.length)
+        return -1;
+      Set<Integer> set = new HashSet<>();
+      for (int i = 0; i < connections.length; i++) {
+        set.add(connections[i][0]);
+        set.add(connections[i][1]);
+      }
+
+      return n - set.size();
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println();
+    Solution s = new Solution();
+  }
+}
