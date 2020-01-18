b26d84109e3f7bd5ed4819e121b7e52b040bb222
gdhu <guodong_hu@126.com>
2020-01-19 06:23:25 +0800

    leet code 0036

--- leetcode/editor/cn/_0036_ValidSudoku_naive.java
+++ leetcode/editor/cn/_0036_ValidSudoku_naive.java
@@ -59,10 +59,13 @@
 
 package leetcode.editor.cn;
 
-public class ValidSudoku {
+import java.util.HashMap;
+import java.util.Map;
+
+public class _0036_ValidSudoku_naive {
 
   public static void main(String[] args) {
-    Solution solution = new ValidSudoku().new Solution();
+    Solution solution = new _0036_ValidSudoku_naive().new Solution();
   }
 
 
@@ -70,7 +73,30 @@ public class ValidSudoku {
   class Solution {
 
     public boolean isValidSudoku(char[][] board) {
-      return false;
+      Map<Integer, Integer>[] rows = new HashMap[9];
+      Map<Integer, Integer>[] cols = new HashMap[9];
+      Map<Integer, Integer>[] boxes = new HashMap[9];
+      for (int i = 0; i < 9; i++) {
+        rows[i] = new HashMap<>();
+        cols[i] = new HashMap<>();
+        boxes[i] = new HashMap<>();
+      }
+
+      for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+          if (board[i][j] != '.') {
+            int n = (int) board[i][j];
+            rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+            cols[j].put(n, cols[j].getOrDefault(n, 0) + 1);
+            int box_idx = (i / 3) * 3 + j / 3;
+            boxes[box_idx].put(n, boxes[box_idx].getOrDefault(n, 0) + 1);
+            if (rows[i].get(n) > 1 || cols[j].get(n) > 1 || boxes[box_idx].get(n) > 1) {
+              return false;
+            }
+          }
+        }
+      }
+      return true;
     }
   }
 //leetcode submit region end(Prohibit modification and deletion)
