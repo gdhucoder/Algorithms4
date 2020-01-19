02089c679c6a89b6c63376dbafe2767d76af30b2
gdhu <guodong_hu@126.com>
2020-01-19 22:03:46 +0800

    leet code weekly competition

---
+++ ALeetCode/comp/p172/Problem1.java
@@ -0,0 +1,35 @@
+package ALeetCode.comp.p172;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem1 {
+
+  static class Solution {
+
+    public int maximum69Number(int num) {
+      String n = "" + num;
+      char[] nums = n.toCharArray();
+      int max = num;
+      for (int i = 0; i < nums.length; i++) {
+        char temp = nums[i];
+        int number = 0;
+        if (nums[i] == '9') {
+          nums[i] = '6';
+        } else {
+          nums[i] = '9';
+        }
+
+        number = Integer.parseInt(new String(nums));
+        max = Math.max(max, number);
+        nums[i] = temp;
+      }
+      return max;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.maximum69Number(9699));
+  }
+}
---
+++ ALeetCode/comp/p172/Problem2.java
@@ -0,0 +1,65 @@
+package ALeetCode.comp.p172;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem2 {
+
+  static class Solution {
+
+    public List<String> printVertically(String s) {
+      List<String> res = new ArrayList<>();
+      String[] words = s.split(" ");
+      int listMaxSize = -1;
+      for (int i = 0; i < words.length; i++) {
+        listMaxSize = Math.max(listMaxSize, words[i].length());
+      }
+      for (int i = 0; i < listMaxSize; i++) {
+        res.add("");
+      }
+
+      for (int i = 0; i < words.length; i++) {
+        String word = words[i];
+        for (int j = 0; j < word.length(); j++) {
+          res.set(j, res.get(j) + word.charAt(j));
+        }
+        for (int j = word.length(); j < listMaxSize; j++) {
+          res.set(j, res.get(j) + " ");
+        }
+      }
+      for (int i = 0; i < res.size(); i++) {
+        res.set(i, trimString(res.get(i)));
+      }
+
+      return res;
+    }
+
+    private String trimString(String last) {
+      if (last.lastIndexOf(' ') != -1) {
+        int limit = last.length() - 1;
+
+        while (limit > 0) {
+          if (last.charAt(limit) == ' ') {
+            limit--;
+          } else {
+            break;
+          }
+        }
+        if (limit >= 0) {
+          last = last.substring(0, limit + 1);
+
+        }
+      }
+      return last;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.printVertically("TO BE OR NOT TO BE"));
+    System.out.println("123  ".indexOf(" "));
+  }
+}
