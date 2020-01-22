c082be96a1e276bac2fe67c7d6df01a41424273d
gdhu <guodong_hu@126.com>
2020-01-22 21:39:49 +0800

    refactor

---
+++ designpattern/u35/IDGenerator.java
@@ -0,0 +1,9 @@
+package designpattern.u35;
+
+/**
+ * Created by HuGuodong on 1/22/20.
+ */
+public interface IDGenerator {
+
+  String generate();
+}
---
+++ designpattern/u35/LogTraceIDGenerator.java
@@ -0,0 +1,7 @@
+package designpattern.u35;
+
+/**
+ * Created by HuGuodong on 1/22/20.
+ */
+public interface LogTraceIDGenerator extends IDGenerator {
+}
