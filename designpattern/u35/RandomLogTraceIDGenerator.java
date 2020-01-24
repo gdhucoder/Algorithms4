0e523da821b8752751af2ba8e792ddb3ec30e42e
gdhu <guodong_hu@126.com>
2020-01-25 00:32:50 +0800

    exception

--- designpattern/u35/RandomLogTraceIDGenerator.java
+++ designpattern/u35/RandomLogTraceIDGenerator.java
@@ -19,6 +19,8 @@ public class RandomLogTraceIDGenerator implements LogTraceIDGenerator {
     try {
       lastField = getLastFieldOfHostName();
     } catch (UnknownHostException e) {
+
+      //
       e.printStackTrace();
     }
     char[] chars = genRandomChars(8);
