8c679d599aef93fbe60189e2d712bfc3985db507
gdhu <guodong_hu@126.com>
2020-01-26 22:07:18 +0800

    add code templates

---
+++ ALeetCode/comp/p175/Problem2.java
@@ -0,0 +1,16 @@
+package ALeetCode.comp.p175;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem2 {
+
+  static class Solution {
+
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println();
+  }
+}
