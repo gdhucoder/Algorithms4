963aabd7c8d96bc712fa36871ed6140d3c008455
gdhu <guodong_hu@126.com>
2020-01-26 20:28:02 +0800

    leetcode weekly competition

---
+++ ALeetCode/comp/p173/Problem1.java
@@ -0,0 +1,32 @@
+package ALeetCode.comp.p173;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem1 {
+
+  static class Solution {
+
+    public int removePalindromeSub(String s) {
+      if (s.length() == 0)
+        return 0;
+      int l = s.length();
+      char[] chars = s.toCharArray();
+      for (int i = 0; i < (l >> 1); i++) {
+        if (chars[i] != chars[l - i - 1])
+          return 2;
+      }
+      return 1;
+    }
+  }
+
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.removePalindromeSub("abb"));
+    System.out.println(s.removePalindromeSub("baabb"));
+    System.out.println(s.removePalindromeSub("ababa"));
+    System.out.println(s.removePalindromeSub(""));
+    System.out.println(s.removePalindromeSub("bbaabaaa"));
+  }
+}
---
+++ ALeetCode/comp/p173/Problem3.java
@@ -0,0 +1,19 @@
+package ALeetCode.comp.p173;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem3 {
+
+  static class Solution {
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println();
+    Solution s = new Solution();
+  }
+}
