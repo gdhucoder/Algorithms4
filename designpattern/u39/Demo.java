8e22a85625b0ac4e2ae7dcb9c7e572591ac35993
gdhu <guodong_hu@126.com>
2020-01-31 22:44:59 +0800

    refactor

--- designpattern/u39/Demo.java
+++ designpattern/u39/Demo.java
@@ -3,6 +3,8 @@ package designpattern.u39;
 import designpattern.u39.reporter.ConsoleReporter;
 import designpattern.u39.storage.MetricsStorage;
 import designpattern.u39.storage.MockRedisMetricsStorage;
+import designpattern.u39.viewer.ConsoleViewer;
+import org.junit.Test;
 
 /**
  * Created by HuGuodong on 1/31/20.
@@ -12,7 +14,8 @@ public class Demo {
 
   public static void main(String[] args) {
     MetricsStorage storage = new MockRedisMetricsStorage();
-    ConsoleReporter consoleReporter = new ConsoleReporter(storage);
+    Aggregator aggregator = new Aggregator();
+    ConsoleReporter consoleReporter = new ConsoleReporter(storage, aggregator, new ConsoleViewer());
     consoleReporter.startRepeatedReport(2, 2);
 
 //    EmailReporter emailReporter = new EmailReporter(storage);
@@ -31,5 +34,33 @@ public class Demo {
 //    } catch (InterruptedException e) {
 //      e.printStackTrace();
 //    }
+  }
+
+  @Test
+  public void testConsoleReporter() {
+    MetricsStorage storage = new MockRedisMetricsStorage();
+    Aggregator aggregator = new Aggregator();
+    ConsoleReporter consoleReporter = new ConsoleReporter(storage, aggregator, new ConsoleViewer());
+    consoleReporter.startRepeatedReport(2, 2);
+    MetricsCollector collector = new MetricsCollector(storage);
+    collector.recordRequest(new RequestInfo("register", 123, 10234));
+    collector.recordRequest(new RequestInfo("register", 223, 11234));
+    collector.recordRequest(new RequestInfo("register", 323, 12334));
+    collector.recordRequest(new RequestInfo("login", 23, 12434));
+    collector.recordRequest(new RequestInfo("login", 1223, 14234));
+  }
+
+  @Test
+  public void testEmailReporter() {
+    MetricsStorage storage = new MockRedisMetricsStorage();
+    Aggregator aggregator = new Aggregator();
+    ConsoleReporter consoleReporter = new ConsoleReporter(storage, aggregator, new ConsoleViewer());
+    consoleReporter.startRepeatedReport(2, 2);
+    MetricsCollector collector = new MetricsCollector(storage);
+    collector.recordRequest(new RequestInfo("register", 123, 10234));
+    collector.recordRequest(new RequestInfo("register", 223, 11234));
+    collector.recordRequest(new RequestInfo("register", 323, 12334));
+    collector.recordRequest(new RequestInfo("login", 23, 12434));
+    collector.recordRequest(new RequestInfo("login", 1223, 14234));
   }
 }
