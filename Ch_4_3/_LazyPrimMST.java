5a1cf8de2e5cd940b088920e38216217c6ed6187
gdhu <guodong_hu@126.com>
2020-04-30 06:28:10 +0800

    alg4

---
+++ Ch_4_3/_LazyPrimMST.java
@@ -0,0 +1,6 @@
+package Ch_4_3;
+
+/**
+ * Created by HuGuodong on 4/30/20.
+ */
+public class _LazyPrimMST {}
---
+++ geekbang/ALGO/w07/TestBit.java
@@ -0,0 +1,11 @@
+package geekbang.ALGO.w07;
+
+/**
+ * Created by HuGuodong on 3/26/20.
+ */
+public class TestBit {
+
+  public static void main(String[] args) {
+    System.out.println(Integer.toBinaryString((5 >> 1) & 1));
+  }
+}
