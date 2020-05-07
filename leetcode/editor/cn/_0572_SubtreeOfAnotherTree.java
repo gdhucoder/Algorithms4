37cae13a2f5ab002a3f444e5a5310b5f73e4a580
gdhu <guodong_hu@126.com>
2020-05-07 21:40:22 +0800

    leetcode

---
+++ leetcode/editor/cn/_0572_SubtreeOfAnotherTree.java
@@ -0,0 +1,98 @@
+//给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看
+//做它自身的一棵子树。 
+//
+// 示例 1: 
+//给定的树 s: 
+//
+// 
+//     3
+//    / \
+//   4   5
+//  / \
+// 1   2
+// 
+//
+// 给定的树 t： 
+//
+// 
+//   4 
+//  / \
+// 1   2
+// 
+//
+// 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。 
+//
+// 示例 2: 
+//给定的树 s： 
+//
+// 
+//     3
+//    / \
+//   4   5
+//  / \
+// 1   2
+//    /
+//   0
+// 
+//
+// 给定的树 t： 
+//
+// 
+//   4
+//  / \
+// 1   2
+// 
+//
+// 返回 false。 
+// Related Topics 树
+
+
+package leetcode.editor.cn;
+
+public class _0572_SubtreeOfAnotherTree {
+
+  public static void main(String[] args) {
+    Solution solution = new _0572_SubtreeOfAnotherTree().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+  /**
+   * Definition for a binary tree node.
+   * public class TreeNode {
+   *     int val;
+   *     TreeNode left;
+   *     TreeNode right;
+   *     TreeNode() {}
+   *     TreeNode(int val) { this.val = val; }
+   *     TreeNode(int val, TreeNode left, TreeNode right) {
+   *         this.val = val;
+   *         this.left = left;
+   *         this.right = right;
+   *     }
+   * }
+   */
+  /**
+   * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+   * right; TreeNode(int x) { val = x; } }
+   */
+  class Solution {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+      if (t == null) return true;
+      if (s == null) return false;
+      return isSameTree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSameTree(TreeNode s, TreeNode t) {
+      if (s == null && t == null) return true;
+      else if (s == null || t == null) return false;
+      else if (s.val == t.val) {
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  //leetcode submit region end(Prohibit modification and deletion)
+
+}
