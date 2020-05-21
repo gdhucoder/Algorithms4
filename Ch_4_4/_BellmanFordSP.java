b3abad60a3210379bfd13c96ac526df00de11f5e
gdhu <guodong_hu@126.com>
2020-05-22 07:33:15 +0800

    leeetcode

---
+++ Ch_4_4/_BellmanFordSP.java
@@ -0,0 +1,14 @@
+package Ch_4_4;
+
+/*
+ * Created by HuGuodong on 2020/5/22.
+ */
+public class _BellmanFordSP {
+
+  /**
+   * queue-based
+   */
+  private double[] distTo; // length of path to v
+  private _DirectedEdge[] edgeTo; // last edge on path to v
+  private boolean[] onQ;
+}
