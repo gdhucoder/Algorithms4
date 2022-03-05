25c794c74f681b6226c415d63eeb4345c908ed4d
gdhu <guodong_hu@126.com>
2022-03-05 08:35:00 +0800

    weekly problems

---
+++ ALeetCode/comp/p270/Problem1.java
@@ -0,0 +1,28 @@
+package ALeetCode.comp.p270;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HuGuodong on 12/8/19.
+ */
+public class Problem1 {
+
+  static class Solution {
+
+    public int[] findEvenNumbers(int[] digits) {
+      List<Integer> res = new ArrayList<>();
+      int len = digits.length;
+      boolean[] v = new boolean[len];
+
+      return null;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println();
+    Short num = -32767;
+    System.out.println(Short.toUnsignedInt(num));
+  }
+}
